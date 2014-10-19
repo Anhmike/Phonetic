19da089dafc7a40a7b709447ec5c7922268c8f10
ryannorton <nort.ryan@gmail.com>
2014-10-19 03:33:58 -0500

    fixed css Safari issue

--- app/views.py
+++ app/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render
-from urllib2 import urlopen
 from django.http import HttpResponse
-import xml.etree.ElementTree as ET
+from nltk.corpus import cmudict
 
 def index(request):
 	return render(request, 'index.html')
 	
 def transcribe(request, format, text):
-	key = "023c06b3-d060-417d-96f0-44201d53f2db"
-	url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + text +"?key=" + key;
-	xml = urlopen(url).read()
-	root = ET.fromstring(xml)
-	transcription = "invalid - try again"
-	for e in root[0]:
-		if e.tag == "pr":
-			transcription = e.text
-			break
+	wordlist = text.split(" ")
+	transcription = ""
+	for word in wordlist:
+		try:
+			phonemes = cmudict.dict()[word.lower()][0]
+			for e in phonemes:
+				transcription += e + "-"
+			transcription = transcription[:-1] + " "
+
+		except:
+			pass
+
+	if transcription == "" and text != "":
+		transcription = "invalid - try again"
+
+	else:
+		transcription = transcription[:-1]
+
 
 	if format == "APA":
 		pass
